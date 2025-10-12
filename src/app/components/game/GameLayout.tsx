03dc07826ee8b9bf83a6922b9211794cfef958c6
IPandragonI <mathysf84pro@gmail.com>
2025-10-12 12:32:37 +0200

    feat: fix player display logic in GameLayout to correctly show player information

--- src/app/components/game/GameLayout.tsx
+++ src/app/components/game/GameLayout.tsx
@@ -21,11 +21,11 @@ const GameLayout: React.FC<GameLayoutProps> = ({
         <div className={`flex ${isGameStarted ? "flex-col-reverse md:flex-row" : "flex-row"} items-start justify-center gap-8 w-full overflow-hidden h-screen p-6`}>
             <div className={`${isGameStarted ? "" : "hidden"} md:flex gap-4 flex-col items-center justify-center md:w-3/5 w-full h-full`}>
                 <div className={`h-full w-full max-w-sm lg:max-w-md 2xl:max-w-lg mx-auto flex flex-col items-center gap-2 justify-start md:justify-between py-8 md:py-0`}>
-                    <GamePlayerInfo isGameStarted={isGameStarted} player={user?.id === whitePlayer?.id ? whitePlayer : blackPlayer}/>
+                    <GamePlayerInfo isGameStarted={isGameStarted} player={user?.id === whitePlayer?.id ? blackPlayer : whitePlayer}/>
                     <div className={"w-full max-w-sm lg:max-w-md 2xl:max-w-lg aspect-square shadow-lg rounded-lg flex items-center justify-center mx-auto p-2 bg-base-200 border border-gray-200"}>
                         {chessBoard}
                     </div>
-                    <GamePlayerInfo isGameStarted={isGameStarted} player={user?.id === whitePlayer?.id ? blackPlayer : whitePlayer}/>
+                    <GamePlayerInfo isGameStarted={isGameStarted} player={user?.id === whitePlayer?.id ? whitePlayer : blackPlayer}/>
                 </div>
             </div>
             <div className={`w-full h-full md:w-2/5 ${isGameStarted ? "hidden" : ""} md:flex`}>
